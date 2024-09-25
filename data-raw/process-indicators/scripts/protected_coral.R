56f2c4e4124ff44b3014a25307765f1bc02eb5b1
cabuelow <christinabuelow@gmail.com>
2024-09-26 09:33:26 +1000

    update coral protected to 1km res

--- data-raw/process-indicators/scripts/protected_coral.R
+++ data-raw/process-indicators/scripts/protected_coral.R
@@ -15,27 +15,25 @@ library(exactextractr)
 # set sf to not use S2
 sf_use_s2(FALSE)
 
-# set terra's temporary folder
-# Replace the path in quotes with path to a temporary folder in your computer
-temp <- "data-raw/process-indicators/data-downloaded/temp"
-terraOptions(tempdir = temp)
+# # change terra's temporary folder to store temporary files
+# # set this path to a folder in your computer if needed
+# temp <- "e:/KC/temp"
+# terraOptions(tempdir = temp)
 
 ## read coral raster file
+# read 1km resolution data, replace with path to new data if needed.
+fils <- list.files("data-raw/process-indicators/data-downloaded/global-coral-reefs-cell-reports-sub/",
+                   full.names = TRUE, pattern = "-1km")
 
-# read 5m resolution data as raster, replace with path to new data if needed.
-fils <- list.files("data-raw/process-indicators/data-downloaded/effective-protection/global-coral-reefs-cell-reports-sub/",
-                   full.names = TRUE, pattern = "_5m")
+coral_1km_rast <- lapply(fils, rast)
 
-coral_5m_rast <- lapply(fils, rast)
-names(coral_5m_rast)
+# function to extract tiles' area from 1km resolution raster
+getvalue <- function(x, y) { # x is spatVector, y is list of raster files
 
-# read country layer
-cty <- vect('data-raw/process-indicators/data-downloaded/targeted_countries_eez_land.gpkg')
-# exclude countries that have no coral
-cty <- cty[!cty$UNION %in% c("Chile", "Alaska", "Peru", "Ecuador"),]
-
-# function to extract coral area from 5m resolution raster (see code at bottom for 1km extraction)
-getvalue <- function(x, y) { # x is country spatVector, y is list of raster files
+  # get extent of tif for intersecting with cty
+  extent <- sapply(y, ext) # extract extent
+  ext_vect <- sapply(extent, vect, crs = crs(x)) # convert extent to a list of SpatVector
+  names(ext_vect) <- 1:length(y)# naming list by using raster list sequence number
 
   df <- data.frame(UNION = NA, area = NA)
 
@@ -43,39 +41,47 @@ getvalue <- function(x, y) { # x is country spatVector, y is list of raster file
 
     x_i <- x[i,]
 
-    index <- grep(x_i$UNION, sapply(y, sources))
+    # to generate an index for rast files that intersect with x_i
+    test <- ext_vect[sapply(ext_vect, is.related, x_i, relation = "intersects") == TRUE]
+    index <- as.numeric(names(test))
+    # select rast
+    if (length(test) > 0) {
+      rast_i <- vrt(fils[index], paste0(temp, "/coral.vrt"), overwrite = T) # select and merge if multiple
 
-    rast_i <- y[index]
+      rast_mask <- crop(rast_i$coral_3, x_i, mask = TRUE) %>%
+        subst(., 0, NA)
 
-    results <- list()
+      if (!is.na(minmax(rast_mask)["max",])) {
 
-    for (j in 1:length(rast_i)) {
-      rast_j <- rast_i[[j]]
-      cell <- cellSize(rast_j, unit = "ha", mask = TRUE, overwrite = TRUE, todisk = TRUE, filename = "data-raw/process-indicators/data-downloaded/temp/c_cell.tif")
-      area <- exact_extract(cell, st_as_sf((x_i)), "sum", max_cells_in_memory = 3e+08)
+        cell <- cellSize(rast_mask, unit = "ha", mask = TRUE, overwrite = TRUE)
 
-      results[[j]] <- area
+        # exactextractr
+        area <- exact_extract(cell, st_as_sf(x_i), "sum")
+
+        df[i,1] <- x_i$UNION
+        df[i,2] <- area
+      }else{
+        df[i,1] <- x_i$UNION
+        df[i,2] <- NA
+      }
     }
-    df[i,1] <- x_i$UNION
-    df[i,2] <- sum(unlist(results), na.rm = TRUE)
   }
-
   return(df)
 }
 
-coral_area <- getvalue(cty, coral_5m_rast)
+## get total coral area
+# read country layer
+cty <- vect('data-raw/process-indicators/data-downloaded/targeted_countries_eez_land.gpkg')
+
+coral_area <- getvalue(cty, coral_1km_rast)
 colnames(coral_area)[2] <- "coral_2018_20_ha"
 
-
 ## get protected area
-
 # read countries' pa layer
 cty_pa <- vect('data-raw/process-indicators/data-downloaded/effective-protection/selected_countries_protected_area.gpkg')
-# exclude countries that have no coral
-cty_pa <- cty_pa[!cty_pa$UNION %in% c("Chile", "Alaska", "Peru", "Ecuador"),]
 
 # get area of protected salt marsh
-coral_pa_area <- getvalue(cty_pa, coral_5m_rast)
+coral_pa_area <- getvalue(cty_pa, coral_1km_rast)
 colnames(coral_pa_area)[2] <- "coral_pa_2018_20_ha"
 
 ## calculate percentage protected
@@ -94,22 +100,17 @@ write.csv(coral, 'data-raw/process-indicators/data-downloaded/effective-protecti
 
 
 ### End of main code ###
-
-# ## Old code to process data using 1km resolution ####
+# # Old code for processing data downloaded using rgee_dim
 #
-# # read 1km resolution data
+# ## read coral raster file
+# # read 1km resolution data, replace with path to new data if needed.
 # fils <- list.files("data/global-coral-reefs-cell-reports-sub/",
-#                    full.names = TRUE, pattern = "1km")
+#                    full.names = TRUE, pattern = "_1km")
 #
 # coral_1km_rast <- lapply(fils, rast)
 #
 # # function to extract tiles' area from 1km resolution raster
 # getvalue <- function(x, y) { # x is spatVector, y is list of raster files
-#
-#   # get extent of tif for intersecting with cty
-#   extent <- sapply(y, ext) # extract extent
-#   ext_vect <- sapply(extent, vect, crs = crs(x)) # convert extent to a list of SpatVector
-#   names(ext_vect) <- 1:length(y)# naming list by using raster list sequence number
 #
 #   df <- data.frame(UNION = NA, area = NA)
 #
@@ -117,35 +118,23 @@ write.csv(coral, 'data-raw/process-indicators/data-downloaded/effective-protecti
 #
 #     x_i <- x[i,]
 #
-#     # to generate an index for rast files that intersect with x_i
-#     test <- ext_vect[sapply(ext_vect, is.related, x_i, relation = "intersects") == TRUE]
-#     index <- as.numeric(names(test))
+#     index <- grep(x_i$UNION, sapply(y, sources))
+#
+#     rast_i <- y[index]
+#
 #     # select rast
-#     if (length(test) > 0) {
-#       rast_i <- vrt(fils[index], paste0(temp, "/coral.vrt"), overwrite = T) # select and merge if multiple
-#       rast_i <- rast_i$coral_3 # select coral extent layer
+#     if (length(rast_i) > 0) {
+#       results <- lapply(rast_i, function(z){
+#         cell <- cellSize(z, unit = "ha", mask = TRUE, overwrite = TRUE, todisk = TRUE)
+#         exact_extract(cell, st_as_sf(x_i), "sum", max_cells_in_memory = 3e+08)})
 #
-#       rast_mask <- crop(rast_i, x_i, mask = TRUE) %>%
-#         subst(., 0, NA)
+#       df[i,1] <- x_i$UNION
+#       df[i,2] <- sum(unlist(results), na.rm = TRUE)
 #
-#       if (!is.na(minmax(rast_mask)["max",])) {
-#
-#         cell <- cellSize(rast_mask, unit = "ha", mask = TRUE, overwrite = TRUE)
-#
-#         # exactextractr
-#         a <- exact_extract(cell, st_as_sf(x_i))[[1]]
-#         area <- sum(a$value * a$coverage_fraction, na.rm = TRUE)
-#
-#         df[i,1] <- x_i$UNION
-#         df[i,2] <- area
-#       }else{
-#         df[i,1] <- x_i$UNION
-#         df[i,2] <- NA
-#       }
+#     }else{
+#       df[i,1] <- x_i$UNION
+#       df[i,2] <- NA
 #     }
 #   }
 #   return(df)
 # }
-#
-# coral_area <- getvalue(cty, coral_1km_rast)
-# colnames(coral_area)[2] <- "coral_2018_20_ha"
